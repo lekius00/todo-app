9b35b9ee92719fa2dd47c20bdc738b9c55e99d93
Hanna <lekamagithub@gmail.com>
2026-06-16 09:13:44 -0300

    feat: Criado arquivo tarefas.py

---
+++ tarefas.py
@@ -0,0 +1,14 @@
+class Tarefas:
+    def __init__(self):
+        self.lista = []
+
+    def adicionar(self, nome):
+        self.lista.append({
+            "nome": nome,
+            "concluida": False
+        })
+
+    def listar(self):
+        for i, t in enumerate(self.lista, 1):
+            s = "✓" if t["concluida"] else "o"
+            print(f"{i}. [{s}] {t['nome']}")
